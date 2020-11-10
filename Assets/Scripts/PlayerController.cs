9e3a57475f2ec1570e686dc530c06e2699889ba4
SharkFin <sharkfinpro@hotmail.com>
2020-11-10 09:46:37 -0800

    Added Grappling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,13 @@
 ///Description: Simple player controller for platformers
 ////////////////
 
+/*
+ * Updated by Alex Martin
+ * 11/10
+ * Added Grappling
+**/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,6 +30,15 @@ public class PlayerController : MonoBehaviour
     private CapsuleCollider2D groundCol;
     private bool onGround;
 
+
+    // Grappling
+    private LineRenderer grappleLine;
+    public Material grappleMaterial;
+    private bool grappling = false;
+    private Vector2 initGrap;
+    private float grapMove;
+    private float grapLength;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,28 +49,113 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        // movement
-        float movement;
-        float horizInput = Input.GetAxisRaw("Horizontal");
-        // move left and right
-        // if pressing left and right, use StartAccel
-        if(horizInput != 0)
+        if (!grappling)
         {
-            // interpolate between current velocity and desired velocity
-            movement = Mathf.Lerp(rb.velocity.x, horizInput * Speed * Time.deltaTime, StartAccel * Time.deltaTime);
+            // movement
+            float movement;
+            float horizInput = Input.GetAxisRaw("Horizontal");
+            // move left and right
+            // if pressing left and right, use StartAccel
+            if (horizInput != 0)
+            {
+                // interpolate between current velocity and desired velocity
+                movement = Mathf.Lerp(rb.velocity.x, horizInput * Speed * Time.deltaTime, StartAccel * Time.deltaTime);
+            }
+            // otherwise use EndAccel
+            else
+            {
+                movement = Mathf.Lerp(rb.velocity.x, horizInput * Speed * Time.deltaTime, EndAccel * Time.deltaTime);
+            }
+            // KEEP Y VELOCITY CONSISTENT!
+            // this is why movement is best kept as just a float instead of a vector
+            if (onGround = true)
+            {
+                rb.velocity = new Vector2(Mathf.Clamp(movement, -MaxSpeed, MaxSpeed), rb.velocity.y);
+            }
         }
-        // otherwise use EndAccel
-        else
+
+        // Grapple
+        if (Input.GetMouseButtonDown(0))
         {
-            movement = Mathf.Lerp(rb.velocity.x, horizInput * Speed * Time.deltaTime, EndAccel * Time.deltaTime);
+            // Check if there is a wall to grapple to
+            Vector2 mousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float pxdif = mousPos.x - getPosition().x;
+            float pydif = mousPos.y - getPosition().y;
+
+            RaycastHit2D hit = Physics2D.Raycast(initGrap +
+                (new Vector2(pxdif, pydif) * groundCol.bounds.size.magnitude)
+                , new Vector2(pxdif, pydif), 100.0f);
+
+            if (hit)
+            {
+                // Create grappling line
+                grappling = true;
+                if (grappleLine == null)
+                {
+                    createGrapple();
+                }
+
+                grapMove = 0;
+                initGrap = this.getPosition();
+                grappleLine.SetPosition(0, initGrap);
+                grappleLine.SetPosition(1, hit.point);
+
+                Vector2 p1 = grappleLine.GetPosition(0);
+                Vector2 p2 = grappleLine.GetPosition(1);
+
+                float xdif = p2.x - p1.x;
+                float ydif = p2.y - p1.y;
+                grapLength = (float)Math.Sqrt(xdif * xdif + ydif * ydif);
+            }
+
         }
-        // KEEP Y VELOCITY CONSISTENT!
-        // this is why movement is best kept as just a float instead of a vector
-        if (onGround = true)
+        else if (Input.GetMouseButton(0) && grappling)
         {
-            rb.velocity = new Vector2(Mathf.Clamp(movement, -MaxSpeed, MaxSpeed), rb.velocity.y);
+            // Grapple Movement
+            if (Input.GetKey(KeyCode.W))
+            {
+                grapMove += grapLength / 25;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                grapMove -= grapLength / 25;
+            }
+            // Stay in bounds of Grapple
+            if (grapMove < 0.0f)
+            {
+                grapMove = 0.0f;
+            }
+            else if (grapMove > grapLength)
+            {
+                grapMove = grapLength;
+            }
+
+            // Grapple movement calculation
+            Vector2 p1 = grappleLine.GetPosition(0); // Point 1
+            Vector2 p2 = grappleLine.GetPosition(1); // Point 2
+
+            float ydif = p2.y - p1.y;
+            float ang = (float)Math.Asin(ydif / grapLength);
+
+            float yd = (float)Math.Sin(ang) * grapMove;
+            float xd = (float)Math.Cos(ang) * grapMove;
+
+            // Move along the grappling line
+            if (p2.x < p1.x)
+            {
+                rb.position = new Vector2(initGrap.x - xd, initGrap.y + yd);
+            }
+            else
+            {
+                rb.position = new Vector2(initGrap.x + xd, initGrap.y + yd);
+            }
+            
         }
-        
+        else if (Input.GetMouseButtonUp(0) && grappling)
+        {
+            Destroy(grappleLine);
+            grappling = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -71,4 +172,20 @@ public class PlayerController : MonoBehaviour
             onGround = false;
         }
     }
+
+    Vector2 getPosition()
+    {
+        return rb.position;
+    }
+
+    void createGrapple()
+    {
+        grappleLine = new GameObject("Grapple").AddComponent<LineRenderer>();
+        grappleLine.material = grappleMaterial;
+        grappleLine.positionCount = 2;
+        grappleLine.startWidth = 0.15f;
+        grappleLine.useWorldSpace = true;
+        grappleLine.numCapVertices = 50;
+    }
+
 }
