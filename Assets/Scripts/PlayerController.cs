e455dc16471f7e520e58693447471f0489571b44
SharkFin <sharkfinpro@hotmail.com>
2020-11-12 08:04:43 -0800

    Update grapple
     - Add max grapple length
     - Add static grapple speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,14 +30,20 @@ public class PlayerController : MonoBehaviour
     private CapsuleCollider2D groundCol;
     private bool onGround;
 
-    
+
     // Grappling
+    private bool grappling = false;
+    private float grapSpeed = 0.25f;
+    private float maxGrapLength = 10.0f;
+
     private LineRenderer grappleLine;
     public Material grappleMaterial;
-    private bool grappling = false;
+    
     private Vector2 initGrap;
-    private float grapMove;
+    private Vector2 grapLoc;
+    private float grapPos;
     private float grapLength;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -81,90 +87,15 @@ public class PlayerController : MonoBehaviour
         // Grapple
         if (Input.GetMouseButtonDown(0))
         {
-            // Check if there is a wall to grapple to
-            Vector2 mousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            float pxdif = mousPos.x - getPosition().x;
-            float pydif = mousPos.y - getPosition().y;
-
-            RaycastHit2D hit = Physics2D.Raycast(initGrap +
-                (new Vector2(pxdif, pydif) * groundCol.bounds.size.magnitude)
-                , new Vector2(pxdif, pydif), 100.0f);
-
-            if (hit)
-            {
-                // Create grappling line
-                grappling = true;
-                if (grappleLine == null)
-                {
-                    createGrapple();
-                }
-
-                grapMove = 0;
-                initGrap = this.getPosition();
-                grappleLine.SetPosition(0, initGrap);
-                grappleLine.SetPosition(1, hit.point);
-
-                Vector2 p1 = grappleLine.GetPosition(0);
-                Vector2 p2 = grappleLine.GetPosition(1);
-
-                float xdif = p2.x - p1.x;
-                float ydif = p2.y - p1.y;
-                grapLength = (float)Math.Sqrt(xdif * xdif + ydif * ydif);
-                
-                // Make sure gravity is not an issue, reset velocity
-                rb.bodyType = RigidbodyType2D.Kinematic;
-                rb.velocity = new Vector2(0, 0);
-            }
-
+            startGrappling();
         }
         else if (Input.GetMouseButton(0) && grappling)
         {
-            // Grapple Movement
-            if (Input.GetKey(KeyCode.W))
-            {
-                grapMove += grapLength / 10;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                grapMove -= grapLength / 10;
-            }
-            // Stay in bounds of Grapple
-            if (grapMove < 0.0f)
-            {
-                grapMove = 0.0f;
-            }
-            else if (grapMove > grapLength)
-            {
-                grapMove = grapLength;
-            }
-
-            // Grapple movement calculation
-            Vector2 p1 = grappleLine.GetPosition(0); // Point 1
-            Vector2 p2 = grappleLine.GetPosition(1); // Point 2
-
-            float ydif = p2.y - p1.y;
-            float ang = (float)Math.Asin(ydif / grapLength);
-
-            float yd = (float)Math.Sin(ang) * grapMove;
-            float xd = (float)Math.Cos(ang) * grapMove;
-
-            // Move along the grappling line
-            if (p2.x < p1.x)
-            {
-                rb.position = new Vector2(initGrap.x - xd, initGrap.y + yd);
-            }
-            else
-            {
-                rb.position = new Vector2(initGrap.x + xd, initGrap.y + yd);
-            }
-
+            updateGrapple();
         }
         else if (Input.GetMouseButtonUp(0) && grappling)
         {
-            Destroy(grappleLine);
-            grappling = false;
-            // regain normal physics
-            rb.bodyType = RigidbodyType2D.Dynamic;
+            destroyGrapple();
         }
     }
 
@@ -198,4 +129,100 @@ public class PlayerController : MonoBehaviour
         grappleLine.numCapVertices = 50;
     }
 
+    void startGrappling()
+    {
+        // Check if there is a wall to grapple to
+        Vector2 mousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float pxdif = mousPos.x - getPosition().x;
+        float pydif = mousPos.y - getPosition().y;
+
+        RaycastHit2D hit = Physics2D.Raycast(initGrap +
+            (new Vector2(pxdif, pydif) * groundCol.bounds.size.magnitude)
+            , new Vector2(pxdif, pydif), 100.0f);
+
+        if (hit)
+        {
+            // Create grappling line
+            if (grappleLine == null)
+            {
+                createGrapple();
+            }
+
+            // Set grapple position
+            grapPos = 0;
+            initGrap = this.getPosition();
+            grapLoc = initGrap;
+            grappleLine.SetPosition(0, initGrap);
+            grappleLine.SetPosition(1, hit.point);
+
+            // Check if the grapple length is within bounds of the set max
+            Vector2 p1 = grappleLine.GetPosition(0);
+            Vector2 p2 = grappleLine.GetPosition(1);
+
+            float xdif = p2.x - p1.x;
+            float ydif = p2.y - p1.y;
+            grapLength = (float)Math.Sqrt(xdif * xdif + ydif * ydif);
+
+            if (grapLength > maxGrapLength)
+            {
+                Destroy(grappleLine);
+                return;
+            }
+
+            // Make sure gravity is not an issue, reset velocity
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            rb.velocity = new Vector2(0, 0);
+
+            // Make sure the player knows it is grappling now
+            grappling = true;
+        }
+    }
+
+    void updateGrapple()
+    {
+        // Grapple Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            grapPos += grapSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            grapPos -= grapSpeed;
+        }
+
+        // Stay in bounds of Grapple
+        if (grapPos < 0.0f)
+        {
+            grapPos = 0.0f;
+        }
+        else if (grapPos > grapLength)
+        {
+            grapPos = grapLength;
+        }
+
+        // Grapple movement calculation
+        Vector2 p1 = initGrap; // Point 1
+        Vector2 p2 = grappleLine.GetPosition(1); // Point 2
+
+        float ydif = p2.y - p1.y;
+        float ang = (float)Math.Asin(ydif / grapLength);
+
+        float yd = (float)Math.Sin(ang) * grapPos;
+        float xd = (float)Math.Cos(ang) * grapPos;
+
+        // Move along the grappling line
+        grapLoc = new Vector2(p2.x < p1.x ? initGrap.x - xd : initGrap.x + xd, initGrap.y + yd);
+        rb.position = grapLoc;
+
+        grappleLine.SetPosition(0, grapLoc);
+    }
+
+    void destroyGrapple()
+    {
+        Destroy(grappleLine);
+        grappling = false;
+
+        // Regain normal physics
+        rb.bodyType = RigidbodyType2D.Dynamic;
+    }
 }
