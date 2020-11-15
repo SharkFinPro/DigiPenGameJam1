c7e3a7988a4f6541e1c0b9d30a640fdca9dd2e01
DevDhawan <71658729+DevDhawan@users.noreply.github.com>
2020-11-15 12:30:29 -0800

    Finish
    Level 4 and 5 added
    lava added
    respawn player added

---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,37 @@
+﻿////////////////
+///By: Dev Dhawan
+///Date: 11/11/20
+///Description: Respawns player and resets Level
+////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    public int level;
+    public string PlayerTag;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag(PlayerTag))
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
